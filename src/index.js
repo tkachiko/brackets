fa184fd3c7018df619d547673616826bc99ddb7c
tkachiko <tkachikos@gmail.com>
2021-02-28 00:27:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const stack = [];
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            const opening = bracketsConfig[j][0];
+            const closing = bracketsConfig[j][1];
+            if (
+                char === opening &&
+                !(opening === closing && opening === stack[stack.length - 1])
+            ) {
+                stack.push(closing);
+            } else if (char === closing) {
+                if (closing === stack.pop()) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    return stack.length === 0;
+};
